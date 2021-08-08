a24b2972cd036b249858784ca68c490fd20abf74
jeffuofa93 <wiederkehr@email.arizona.edu>
2021-08-08 13:54:43 -0700

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(express.json());
+app.use(express.static("build"));
 app.use(cors());
 
 morgan.token("body", function getId(request) {
