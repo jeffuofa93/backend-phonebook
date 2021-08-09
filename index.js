6553eccfa251ed9f51dc1a1109298f00b8d2326d
jeffuofa93 <wiederkehr@email.arizona.edu>
2021-08-08 20:52:55 -0700

    uibuild

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const Person = require("./models/person");
 const morgan = require("morgan");
-const cors = require("cors");
 
-app.use(express.json());
 app.use(express.static("build"));
+app.use(express.json());
 app.use(cors());
 
-morgan.token("body", function getId(request) {
-  return JSON.stringify(request.body);
-});
+morgan.token("body", (request) => JSON.stringify(request.body));
+// morgan.token("error", (request) => JSON.stringify(request.body));
 // :method :url :status :res[content-length] - :response-time ms
 // app.use(morgan("tiny"));
 app.use(
   morgan(":method :url :status :res[content-length] :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Popdpendieck",
-    number: "39-23-6423122",
-  },
-];
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  console.log("error handler function");
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((person) => {
@@ -47,10 +32,16 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) response.json(person);
+      else response.status(404).end();
+    })
+    .catch((error) => {
+      console.log("WAT IS THIS");
+      next(error);
+    });
 });
 
 app.get("/api/info", (request, response) => {
@@ -60,41 +51,13 @@ app.get("/api/info", (request, response) => {
   response.send(Buffer.from(`<p>${sizeString}</p><p>${date}</p>`));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-
-// app.post("/api/persons", (request, response) => {
-//   const body = request.body;
-//
-//   const duplicateName = persons.find((person) =>
-//     person.name.includes(body.name)
-//   );
-//
-//   if (!body.name || !body.number) {
-//     return response.status(400).json({
-//       error: "name or number is missing",
-//     });
-//   }
-//
-//   if (duplicateName) {
-//     return response.status(400).json({
-//       error: "name must be unique",
-//     });
-//   }
-//
-//   const person = {
-//     id: randomNumber(0, 100000),
-//     name: body.name,
-//     number: body.number,
-//   };
-//
-//   persons = persons.concat(person);
-//
-//   response.json(person);
-// });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
@@ -112,6 +75,14 @@ app.post("/api/persons", (request, response) => {
     response.json(savedPerson);
   });
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
